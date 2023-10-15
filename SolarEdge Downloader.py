242fd51ae0ff40d55ad1d1fbc16c9a7683143598
Alessio Rossini <72096203+AxReds@users.noreply.github.com>
2023-10-15 16:41:39 +0200

    Selectio Menu and Energy production added to main script and SolarEdge_functions.py

--- SolarEdge Downloader.py
+++ SolarEdge Downloader.py
@@ -2,8 +2,7 @@
 
 import requests
 from datetime import datetime, timedelta
-import os
-import json
+import json, os
 from SolarEdge_functions import *
 
 # Read information from the JSON file
@@ -17,8 +16,6 @@ SolarEdge_SiteID = SolarEdgeConfig['SiteID']
 SolarEdge_ApiKey = SolarEdgeConfig['ApiKey']
 
 #Set other constants
-Day = "DAY"
-QuarterHour = "QUARTER_OF_AN_HOUR"
 CurrentDate = datetime.now().strftime('%Y-%m-%d')
 StartDate = CurrentDate
 EndDate = CurrentDate
@@ -28,41 +25,73 @@ EnergyUrl = f"https://monitoringapi.solaredge.com/site/{SolarEdge_SiteID}/energy
 DetailsUrl = f"https://monitoringapi.solaredge.com/site/{SolarEdge_SiteID}/details?api_key={SolarEdge_ApiKey}"
 PowerDetailsUrl = f"https://monitoringapi.solaredge.com/site/{SolarEdge_SiteID}/powerDetails?api_key={SolarEdge_ApiKey}"
 
-
-#
-# Get site details and last update time from the overview API endpoint
-#response_details = requests.get(OverviewUrl)
-SiteDetails = requests.get(DetailsUrl).json()
-
-response_Overview = requests.get(OverviewUrl).json()
-SiteOverview = response_Overview['overview']
-
-LastUpdateTime = SiteOverview['lastUpdateTime'].split()[0]
-LastUpdateDateTime = datetime.strptime(LastUpdateTime, '%Y-%m-%d')
-
-# Check if data needs to be downloaded based on last update time
-if LastUpdateTime != CurrentDate:
-    DaysAgo = (datetime.now() - LastUpdateDateTime).days
-    if DaysAgo == 0:
-        print("The energy data on the monitoring portal is up to date, will start downloading data if necessary")
-    elif DaysAgo == 1:
-        print("The energy data on the monitoring portal has not been updated for a day. "
-              "This might have various reasons:\n"
-              " - The update script is run within the first 15 minutes of a new day, in that case, it's usual behavior.\n"
-              " - There has been an interruption in the connection between the inverter and the monitoring portal.\n"
-              " - No data has been received on the monitoring portal.\n"
-              "Script will continue as usual.")
-    else:
-        print(f"The installation has not been updated since {LastUpdateTime}. This is {DaysAgo} days ago. "
-              "Script will continue, but it is advised to fix the error. "
-              "This script will only download the data until the date of the last available data")
-
 # Get data period information from the API endpoint
 response_period = requests.get(DataPeriodUrl)
 DataPeriod = response_period.json()
 StartYear = datetime.strptime(DataPeriod['dataPeriod']['startDate'], '%Y-%m-%d').year
 CurrentYear = datetime.now().year
 
-#getAllData (DataPeriod, StartYear, CurrentYear, SiteDetails, SolarEdge_SiteID, EnergyUrl, LastUpdateTime, LastUpdateDateTime)
-getAllDataPVOutFormat (SolarEdge_ExportFile, DataPeriod, StartYear, CurrentYear, PowerDetailsUrl, LastUpdateDateTime)
-print("Export Completed")
+#
+# Get site details and last update time from the overview API endpoint
+#response_details = requests.get(OverviewUrl)
+SiteDetails = requests.get(DetailsUrl).json()
+
+SolarEdge_SiteName = SiteDetails['details']['name']
+SolarEdge_Status = SiteDetails['details']['status']
+SolarEdge_AccountID = SiteDetails['details']['accountId']
+SolarEdge_Status = SiteDetails['details']['status']
+SolarEdge_PeakPower = SiteDetails['details']['peakPower']
+SolarEdge_installationDate = SiteDetails['details']['installationDate']
+SolarEdge_Currency = SiteDetails['details']['currency']
+
+response_Overview = requests.get(OverviewUrl).json()
+SiteOverview = response_Overview['overview']
+SolarEdge_lifeTimeData_Energy = SiteOverview['lifeTimeData']['energy']
+SolarEdge_lifeRevenue_Energy = SiteOverview['lifeTimeData']['revenue']
+
+LastUpdateTime = SiteOverview['lastUpdateTime'].split()[0]
+LastUpdateDateTime = datetime.strptime(LastUpdateTime, '%Y-%m-%d')
+
+
+os.system("cls" if os.name == "nt" else "clear")
+print(f"\n\nSolareEdge Site {SolarEdge_SiteID} owned by {SolarEdge_SiteName} has the following details:\n"
+        f" - Account ID:     {SolarEdge_AccountID}\n"
+        f" - Status:         {SolarEdge_Status}\n"
+        f" - Peak Power:     {SolarEdge_PeakPower}\n"
+        f" - Installed on:   {SolarEdge_installationDate}\n"
+        f" - Total Energy:   {SolarEdge_lifeTimeData_Energy}\n"
+        f" - Total Revenues: {SolarEdge_lifeRevenue_Energy} {SolarEdge_Currency} \n"
+        "\n")
+
+choice = input("Please select an export method and press <enter> to confirm:\n"
+        "1 - Export Daily Power Production (15mins interval)\n"
+        "2 - Export Daily Energy Production (15mins interval)\n"
+        "\n")
+
+if choice == "1":
+    #getAllData (DataPeriod, StartYear, CurrentYear, SiteDetails, SolarEdge_SiteID, EnergyUrl, LastUpdateTime, LastUpdateDateTime)
+    getAllDataPVOutFormat (choice, SolarEdge_ExportFile, DataPeriod, StartYear, CurrentYear, PowerDetailsUrl, LastUpdateDateTime)
+    print("Export Completed")
+elif choice == "2":
+    getAllDataPVOutFormat (choice, SolarEdge_ExportFile, DataPeriod, StartYear, CurrentYear, EnergyUrl, LastUpdateDateTime)
+    print("Export Completed")
+elif choice == "3":
+    print("Il pianeta è stato distrutto!")
+    # Inserisci qui il codice per l'opzione 3
+else:
+    # Check if data needs to be downloaded based on last update time
+    if LastUpdateTime != CurrentDate:
+        DaysAgo = (datetime.now() - LastUpdateDateTime).days
+        if DaysAgo == 0:
+            print("The energy data on the monitoring portal is up to date, will start downloading data if necessary")
+        elif DaysAgo == 1:
+            print("The energy data on the monitoring portal has not been updated for a day. "
+                "This might have various reasons:\n"
+                " - The update script is run within the first 15 minutes of a new day, in that case, it's usual behavior.\n"
+                " - There has been an interruption in the connection between the inverter and the monitoring portal.\n"
+                " - No data has been received on the monitoring portal.\n"
+                "Script will continue as usual.")
+        else:
+            print(f"The installation has not been updated since {LastUpdateTime}. This is {DaysAgo} days ago. "
+                "Script will continue, but it is advised to fix the error. "
+                "This script will only download the data until the date of the last available data")
--- SolarEdge_functions.py
+++ SolarEdge_functions.py
@@ -3,29 +3,20 @@ import requests
 import csv
 from datetime import datetime, timedelta
 import os
-import json
 
 #Set Constants
 Day = "DAY"
 QuarterHour = "QUARTER_OF_AN_HOUR"
 
+#
 # This block is executed when the module is run directly
 if __name__ == "__main__":
     # Test some examples
     print ("This is a library and cannot be ran standalone.")
 
-# Function to get energy data and remove rows with null values
-def get_energy_data_remove_nulls(url, file_name):
-    response = requests.get(url)
-    energy_data = response.json()
-    data = [row['value'] for row in energy_data['energy']['values'] if row['value'] is not None]
-    with open(file_name, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=';')
-        csvwriter.writerow(["Date/Time;Value"])
-        csvwriter.writerows([[row] for row in data])
-
+#
 # Function to get production data and replace null values with "0,0"
-def get_production_data(url, file_name):
+def get_Power_data(url):
     
     #Initialize an empty list to store multiline data
     multiline_data = []
@@ -54,6 +45,11 @@ def get_production_data(url, file_name):
         data = [date, value]
         multiline_data.append(data)
     
+    return (multiline_data)
+
+#
+# Function to get production data and replace null values with "0,0"
+def SolarEdge_ExportToFile(file_name, multiline_data): 
     #
     # Write data to the CSV file
     if not os.path.exists(file_name):
@@ -66,59 +62,48 @@ def get_production_data(url, file_name):
             csvwriter = csv.writer(csvfile, delimiter=';')
             #csvwriter.writerow(['Date/Time','Value']) # DOES NOT Write header if the file EXIST
             csvwriter.writerows(multiline_data)
-            
 
-# Original Extraction Method
-def getAllData (DataPeriod, 
-                StartYear, 
-                CurrentYear, 
-                SiteDetails, 
-                SolarEdge_SiteID, 
-                EnergyUrl,
-                LastUpdateTime,
-                LastUpdateDateTime):
-    for year in range(StartYear, CurrentYear + 1):
-        if year == CurrentYear:
-            if StartYear == CurrentYear:
-                month = datetime.strptime(DataPeriod['dataPeriod']['startDate'], '%Y-%m-%d').month
-            else:
-                month = 1
-            last_month = datetime.now().month if LastUpdateDateTime.month >= datetime.now().month else LastUpdateDateTime.month + 1
+#
+# Function to get production data and replace null values with "0,0"
+def get_Energy_data(url):
+    
+    #Initialize an empty list to store multiline data
+    multiline_data = []
+    
+    # Make a GET request to the specified URL
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code != 200:
+        print(f"Error in the request: {response.status_code}")
+        return
+
+    # Parse the JSON response
+    energy_data = response.json()
+    
+    # Access nested data in the JSON
+    meter_telemetries = energy_data['energy']['values']
+
+    #
+    # Replace null values with "0.0" and create multiline data
+    for meter_telemetry in meter_telemetries:
+        date = meter_telemetry['date']
+        if meter_telemetry['value'] is None:
+            value = 0.0
         else:
-            if year == StartYear:
-                month = datetime.strptime(DataPeriod['dataPeriod']['startDate'], '%Y-%m-%d').month
-            else:
-                month = 1
-            last_month = 13
+            value = meter_telemetry['value']
 
-        for month in range(month, last_month):
-            last_day_of_month = (datetime(year, month % 12 + 1, 1) - timedelta(days=1)).day
-            str_month = "{:02d}".format(month)
-            file_name = f"{year}{str_month} - {SiteDetails['details']['name']}{SolarEdge_SiteID}.csv"
-            file_name_15min = f"{year}{str_month} - {SiteDetails['details']['name']}{SolarEdge_SiteID}-15min.csv"
-            url_day = f"{EnergyUrl}&timeUnit={Day}&startDate={year}-{str_month}-01&endDate={year}-{str_month}-{last_day_of_month}"
-            url_15min = f"{EnergyUrl}&timeUnit={QuarterHour}&startDate={year}-{str_month}-01&endDate={year}-{str_month}-{last_day_of_month}"
-            
-            if not os.path.exists(file_name):
-                get_energy_data_remove_nulls(url_day, file_name)
-            else:
-                print(f"File {file_name} already exists. Script will not download this data again")
-            
-            if not os.path.exists(file_name_15min):
-                get_energy_data_replace_nulls(url_15min, file_name_15min)
-            else:
-                print(f"File {file_name_15min} already exists. Script will not download this data again")
+        
+        # Create a multiline list with 'date' and 'value' and append it to multiline_data
+        data = [date, value]
+        multiline_data.append(data)
+    
+    return (multiline_data)
 
-        if year < CurrentYear and year >= LastUpdateDateTime.year:
-            url_year = f"{EnergyUrl}&timeUnit={Day}&startDate={year}-01-01&endDate={year}-12-31"
-            file_name_year = f"{year} - {SiteDetails['details']['name']}{SolarEdge_SiteID}.csv"
-            if not os.path.exists(file_name_year):
-                get_energy_data_remove_nulls(url_year, file_name_year)
-            else:
-                print(f"File {file_name_year} already exists. Script will not download this data again")
 
+#
 #New Data Extraction Method
-def getAllDataPVOutFormat (ExportFile, DataPeriod, StartYear, CurrentYear, EnergyUrl, LastUpdateDateTime):
+def getAllDataPVOutFormat (Operation, ExportFile, DataPeriod, StartYear, CurrentYear, EnergyUrl, LastUpdateDateTime):
     for year in range(StartYear, CurrentYear + 1):
         if year == CurrentYear:
             if StartYear == CurrentYear:
@@ -136,12 +121,18 @@ def getAllDataPVOutFormat (ExportFile, DataPeriod, StartYear, CurrentYear, Energ
         for month in range(month, last_month):
             last_day_of_month = (datetime(year, month % 12 + 1, 1) - timedelta(days=1)).day
             str_month = "{:02d}".format(month)
+            
+            #Check what exporting process to execute
+            if Operation == "1":
+                url_15min = f"{EnergyUrl}&meters=PRODUCTION&timeUnit={QuarterHour}&startTime={year}-{str_month}-01 00:00:00&endTime={year}-{str_month}-{last_day_of_month} 23:59:59"
+                SolarEdge_DataDictionary = get_Power_data(url_15min)
+            elif Operation == "2":
+                url_15min = f"{EnergyUrl}&timeUnit={QuarterHour}&startDate={year}-{str_month}-01&endDate={year}-{str_month}-{last_day_of_month}"
+                SolarEdge_DataDictionary = get_Energy_data(url_15min)
+            else:
+                print ("Option not yet available")
+            
+            #Export data to file
+            SolarEdge_ExportToFile (ExportFile, SolarEdge_DataDictionary)
 
-
-            url_15min = f"{EnergyUrl}&meters=PRODUCTION&timeUnit={QuarterHour}&startTime={year}-{str_month}-01 00:00:00&endTime={year}-{str_month}-{last_day_of_month} 23:59:59"
-            get_production_data(url_15min, ExportFile)
-
-            #if not os.path.exists(ExportFile):
-            #    get_production_data(url_15min, ExportFile)
-            #else:
-            #    print(f"File {ExportFile} already exists. Script will not download this data again")
+            
